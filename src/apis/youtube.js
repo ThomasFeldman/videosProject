229fa004b658814b993b32b3cf064d1e664e80c9
ThomasFeldman <tfeldman1.31@gmail.com>
2020-03-26 00:25:47 -0500

    adding youtube data api to search for videos

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+inport axios from 'axios';
+
+const KEY = 'AIzaSyCUlXjfsEBIrPiB7ab-0pj1Rv6vcxUZAuY';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+      part: 'snippet',
+      maxResults: 5,
+      key: KEY
+  }
+});
