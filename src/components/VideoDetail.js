b84266ea93001a8a8a9994c9de83ab18650e56d3
ThomasFeldman <tfeldman1.31@gmail.com>
2020-03-28 14:24:15 -0500

    styling video description

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const VideoDetail = ({ video }) => {
-    if (video === null) {
+    if (!video) {
         return <div>Loading...</div>;
-    } else{
-    return <div>{video.snippet.title}</div>;
     }
+    return ( 
+      <div>
+        <div className="ui segment">
+          <h4 className="ui header">{video.snippet.title}</h4>
+          <p>{ video.snippet.description }</p>
+        </div>
+      </div>
+    );
 };
 
 export default VideoDetail;
